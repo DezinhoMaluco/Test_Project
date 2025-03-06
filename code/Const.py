35f443b0dfdb289ad5b96785b66584949d3e23c1
André <andreluisserpadossantos@gmail.com>
2025-03-06 15:19:58 -0300

    Creating asset folder Inserting all assetrs Creating the menu: background, sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,13 @@
+# C
+COLOR_ORANGE = (255, 128, 0)
+COLOR_WHITE = (255, 255, 255)
+
+# M
+MENU_OPTION = ('PLAY',
+               'MODE: RIGHT-HANDED',
+               'SOCRE',
+               'EXIT')
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324

f8149d926bf803415f9e1fcad964e6eb079049ee
André <andreluisserpadossantos@gmail.com>
2025-03-10 17:32:19 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- code/Const.py
+++ code/Const.py
@@ -1,11 +1,22 @@
 # C
+COLOR_BLACK = (0, 0, 0)
+COLOR_PURPLE = (240, 0, 255)
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 
+# E
+ENTITY_SPEED = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 1,
+    'Level1Bg2': 2,
+    'Level1Bg3': 4,
+    'Level1Bg4': 6,
+    'Level1Bg5': 0,
+}
 # M
 MENU_OPTION = ('PLAY',
                'MODE: RIGHT-HANDED',
-               'SOCRE',
+               'SCORE',
                'EXIT')
 
 # W
--- code/menu.py
+++ code/menu.py
@@ -4,7 +4,7 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_PURPLE, COLOR_BLACK
 
 
 class Menu:
@@ -14,23 +14,47 @@ class Menu:
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self, ):
+        menu_option = 0
         pygame.mixer_music.load('./asset/MenuSong.mp3')
         pygame.mixer_music.play(-1)
         while True:
             self.window.blit(source=self.surf, dest=self.rect)
-            self.menu_text(50, "RUN", COLOR_ORANGE, ((WIN_WIDTH / 2), 70))
-            self.menu_text(50, "HANDMAN!", COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(60, "RUN", COLOR_ORANGE, ((WIN_WIDTH / 2), 80))
+            self.menu_text(60, "HANDMAN!", COLOR_ORANGE, ((WIN_WIDTH / 2), 140))
+            self.menu_text(11, "You are a bored kid in the", COLOR_BLACK, (95, 43))
+            self.menu_text(11, "backseat in a travel, when", COLOR_BLACK, (95, 56))
+            self.menu_text(11, "you have the brilliant idea.", COLOR_BLACK, (95, 69))
+            self.menu_text(10, "Created by: André Luís Serpa dos Santos ", COLOR_BLACK, (450, 43))
+            self.menu_text(10, "A.K.A: Dezinho Maluco.", COLOR_BLACK, (450, 56))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 30 * i))
-
-            pygame.display.flip()
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_PURPLE, ((WIN_WIDTH / 2), 200 + 30 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], COLOR_WHITE, ((WIN_WIDTH / 2), 200 + 30 * i))
 
             # Check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # Close Window
                     quit()  # end pygame
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+
+                    if event.key == pygame.K_RETURN:  # Enter
+                        return MENU_OPTION[menu_option]
+
+            pygame.display.flip()
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Comic Sans MS", size=text_size)

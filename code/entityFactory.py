4ba913f5fa7f5afb546a84247fa065a80661e1ff
André <andreluisserpadossantos@gmail.com>
2025-03-12 18:49:19 -0300

    Creating menu interations Creating ABC class Creating Factory Level 1 Bg implementation: parallax

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from code.Const import WIN_WIDTH
 from code.background import Background
+from code.player import Player
 
 
 class EntityFactory:
@@ -15,3 +16,9 @@ class EntityFactory:
                     list_bg.append(Background(f'Level1Bg{i}', (0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+            case 'Player':
+                list_player = []
+                for i in range(3):
+                    list_player.append(Player(f'Player{i}', (-130, 50)))
+                    list_player.append(Player(f'Player{i}', (-130, 50)))
+                return list_player
--- code/level.py
+++ code/level.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import sys
+
 import pygame
 
 from code.entity import Entity
@@ -13,11 +15,20 @@ class Level:
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
         self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
+        self.entity_list.append(EntityFactory.get_entity('Player'))
 
-    def run(self):
+    def run(self, get_rect=None):
+        pygame.mixer_music.load(f'./asset/leap.wav')
+        pygame.mixer_music.play(-1)
+        clock = pygame.time.Clock()
         while True:
+            clock.tick(60)
             for ent in self.entity_list:
                 self.window.blit(source=ent.surf, dest=ent.rect)
                 ent.move()
             pygame.display.flip()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
         pass
